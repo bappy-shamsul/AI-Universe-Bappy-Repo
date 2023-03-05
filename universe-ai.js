a78b49f6c4edb75b538eeaf0986da5b6db9de4b7
Yusha <faizan.mashuq@gmail.com>
2023-03-05 19:17:57 +0600

    forEach prb is not solved yet

--- universe-ai.js
+++ universe-ai.js
@@ -4,7 +4,7 @@
 //     .then(data => console.log(data))
 // }
 
-const loadInformation = async() =>{
+const loadInformation = async () => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`
     const res = await fetch(url)
     const data = await res.json()
@@ -12,11 +12,25 @@ const loadInformation = async() =>{
     // console.log(data.data.tools[0].name);
 }
 
-const displayTools = tools =>{
+const displayTools = tools => {
     console.log(tools);
     const toolContainer = document.getElementById('tool-container')
-    tools.forEach(tool =>{
-
+    tools.forEach(tool => {
+        const toolDiv = document.createElement('div');
+        toolDiv.classList.add('col');
+        toolDiv.innerHTML = `
+    <div class="card h-100">
+      <img src="${tool.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Features</h5>
+        console.log(tool.features)
+        <p class="card-text">${tool.features}</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>`;
+    toolContainer.appendChild(toolDiv);
     })
 }
 
